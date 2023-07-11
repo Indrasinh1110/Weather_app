9e997b8efd61a6587d2fa3f13b7d74fd107ea0ff
Indrasinh <indrasinhmahera1110@gmail.com>
2023-07-12 00:09:13 +0530

    add favicon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-
 import './App.css';
 import HomeComponents from './Components/Home';
-
 function App() {
   return (
     <div className="App">
@@ -9,17 +7,7 @@ function App() {
     </div>
   );
 }
-
 export default App;
-// Manifest
-// Service worker
-// https://
-// favicon.icon
-// maskable icon
-// APP name
-// install swr
-// npx create-react-app my-app --template cra-template-pwa
-
 
 
 
